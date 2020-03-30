6ce7b317f1f207f92405ad00021772acbcc14b8a
Oliver Switzer <oliverswitzer@gmail.com>
2020-03-29 23:39:01 -0400

    Add list view with hard coded places and their popular times
    NOTES:
    * Only have tested the android build locally
    * Used react-native-dotenv to specify api token for python places API
    * Name of the app is still "client" -- need to figure out how to change
    this...

--- client/App.js
+++ client/App.js
@@ -1,114 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, SectionList, Text, View } from 'react-native';
+import { gateway } from "./Gateway";
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = () => {
+  const [places, setPlaces] = useState([]);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  useEffect(() => {
+    const fetchData = async () => {
+      const mrPlum = await gateway.getPlace('ChIJ4RQESkRZwokRzOSbHg_AHd8');
+      const associated = await gateway.getPlace('ChIJ-VYyzkZZwokRLaDlVih5Qzs');
+      const met = await gateway.getPlace('ChIJGfDg705ZwokRvqa_3iQAPsQ');
+      const chase = await gateway.getPlace('ChIJ_2Ukz0ZZwokRJZSgVQYl2eo');
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+      setPlaces([associated, met, mrPlum, chase])
+    };
+    fetchData()
+  }, []);
+
+  return <SafeAreaView>
+    <SectionList
+      renderItem={({ item }) => <Place item={item}/>}
+      renderSectionHeader={() => <Header/>}
+      sections={[{ data: places }]}
+    />
+  </SafeAreaView>;
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Header = () => <Text style={{ fontSize: 20, padding: 12 }}>Top Places Near You</Text>;
+
+const Place = ({ item }) => <View style={{ borderColor: 'gray', borderWidth: 1, padding: 12, margin: 12, marginBottom: 0 }}>
+  <Text style={{fontWeight: 'bold', fontSize: 12}}>{item.name}</Text>
+  <Text>{item.id}</Text>
+  <Text>{JSON.stringify(item.types)}</Text>
+  <View style={{flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between'}}>
+    {item.populartimes[0].data.map((popularity) => <View><View style={{height: popularity, marginRight: 4, backgroundColor: 'blue', fontSize: 2}}/><Text>{popularity}</Text></View>)}
+  </View>
+</View>;
 
 export default App;
---
+++ client/Gateway.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { PLACES_API_TOKEN } from 'react-native-dotenv'
+// import { fixtureData } from "./fixture";
+
+class Gateway {
+
+  _a = axios.create({
+    baseURL: 'https://corona-errands-helper.herokuapp.com',
+    timeout: 1000,
+    headers: {'Authorization': `Token ${PLACES_API_TOKEN}`}
+  });
+
+  getPlace = (id) =>
+    this._a.get(`/places/${id}`)
+      .then(response => response.data)
+
+}
+
+export const gateway = new Gateway();
