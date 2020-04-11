d3f3046c259df3ee42462a0cddc192aa8f8e0a8f
Oliver Switzer <oliverswitzer@gmail.com>
2020-04-11 16:18:54 -0400

    Add nice coloring and adjust the grid so each place is equally aligned

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, SectionList, Text, View } from 'react-native';
+import { Picker, SafeAreaView, SectionList, Text, View } from 'react-native';
 import { gateway, PlaceType } from "./Gateway";
 
 
@@ -8,10 +8,10 @@ const App = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const mrPlum = await gateway.getPlace('ChIJ4RQESkRZwokRzOSbHg_AHd8');
-      const associated = await gateway.getPlace('ChIJ-VYyzkZZwokRLaDlVih5Qzs');
-      const met = await gateway.getPlace('ChIJGfDg705ZwokRvqa_3iQAPsQ');
-      const chase = await gateway.getPlace('ChIJ_2Ukz0ZZwokRJZSgVQYl2eo');
+        const mrPlum = await gateway.getPlace('ChIJ4RQESkRZwokRzOSbHg_AHd8');
+        const associated = await gateway.getPlace('ChIJ-VYyzkZZwokRLaDlVih5Qzs');
+        const met = await gateway.getPlace('ChIJGfDg705ZwokRvqa_3iQAPsQ');
+        const chase = await gateway.getPlace('ChIJ_2Ukz0ZZwokRJZSgVQYl2eo');
 
       setPlaces([associated, met, mrPlum, chase])
     };
@@ -19,6 +19,7 @@ const App = () => {
   }, []);
 
   return <SafeAreaView>
+    <Picker/>
     <SectionList
       renderItem={({ item, index }: { item: PlaceType, index: number}) => <Place item={item} key={index} />}
       renderSectionHeader={() => <Header/>}
@@ -29,23 +30,30 @@ const App = () => {
 
 const Header = () => <Text style={{ fontSize: 20, padding: 12 }}>Top Places Near You</Text>;
 
-const Place = ({ item }: { item: PlaceType }) => <View style={{ borderColor: 'gray', borderWidth: 1, padding: 12, margin: 12, marginBottom: 0 }}>
-  <Text style={{fontWeight: 'bold', fontSize: 12}}>{item.name}</Text>
-  <Text>Rating: {item.rating}</Text>
-  <View style={{flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between'}}>
-    {
-      item.populartimes[1].data.map((popularity: number, index: number) => (
-        <View key={index}>
-          {popularity !== 0 && (
-            <>
-              <View style={{ height: popularity, marginRight: 5, backgroundColor: 'blue' }}/>
-              <Text>{index}</Text>
-            </>
-          )}
-        </View>
-      ))
-    }
-  </View>
-</View>;
+const Place = ({ item}: { item: PlaceType }) => {
+  const popularTimes = item.populartimes[1].data;
+
+  const isLocalMinimum = (index: number): boolean => {
+    const [before, current, after] = [popularTimes[index-1], popularTimes[index], popularTimes[index+1]];
+
+    return (current <= before || !before) &&
+      (current <= after || !after);
+  };
+
+  return <View style={{ borderColor: 'gray', borderWidth: 1, padding: 12, margin: 12, marginBottom: 0 }}>
+    <Text style={{ fontWeight: 'bold', fontSize: 12 }}>{item.name}</Text>
+    <Text>Rating: {item.rating}</Text>
+    <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between' }}>
+      {
+        popularTimes.map((popularity: number, index: number) => (
+          <View key={index} style={{ flex: 1 }}>
+            <View style={{ height: popularity, marginRight: 5, backgroundColor: isLocalMinimum(index) ? 'rgba(0,240,79,0.69)' : '#0096ff' }}/>
+            <Text style={{ fontSize: 10, lineHeight: 13, height: 10 }}>{(index % 3) == 0 && (index + 1) % 12}</Text>
+          </View>
+        ))
+      }
+    </View>
+  </View>;
+};
 
 export default App;
